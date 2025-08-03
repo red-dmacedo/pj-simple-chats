b2e9f14aa9b7660972ab83b057729f822410c672
dmacedo <devinm3333@outlook.com>
2025-08-03 02:59:04 -0700

    completed CRUD on messages

--- public/scripts/message-buttons.js
+++ public/scripts/message-buttons.js
@@ -1,6 +1,21 @@
 const messageAreaEl = document.querySelector('.message-container');
 let previousEl;
 
+const modal = {
+  el: document.querySelector('#edit-message-modal'),
+  closeBtnEl: document.querySelector('#edit-message-modal #close-btn-for-edit-message'),
+  updateBtnEl: document.querySelector('#edit-message-modal #update-btn'),
+  contentEl: document.querySelector('#edit-message-modal #edit-content'),
+  headerEl: document.querySelector('#edit-message-modal #edit-message-header'),
+  formEl: document.querySelector('#edit-message-modal form'),
+};
+
+const userId = document.querySelector('#userId').textContent;
+const targetConvId = document.querySelector('#targetConvId').textContent;
+let lastMsgId = "";
+
+modal.closeBtnEl.addEventListener("click", (evt) => { modal.el.close(); });
+
 messageAreaEl.addEventListener("click", (evt) => {
   function classListContainsAny(classList, diffArr) { // true if any item within diffArr is within classList
     for (item of diffArr) { if (classList.contains(item)) return true; };
@@ -28,11 +43,29 @@ messageAreaEl.addEventListener("click", (evt) => {
   const classesToMatch = ['message', 'message-username', 'message-content', 'message-timestamp'];
   let messageEl;
 
-  if (!classListContainsAny(target.classList, classesToMatch)) return;
+  if (
+    !classListContainsAny(target.classList, classesToMatch) &&
+    target.id !== 'edit-btn'
+  ) return;
   if (classListContainsAny(target.classList, classesToMatch.slice(0, 1))) { messageEl = target; }
   else { messageEl = target.parentNode; };
 
-  if (previousEl !== messageEl) {
+  if (target.id === 'edit-btn') { // show edit modal
+    const msgIdEl = messageEl.querySelector(".message-id");
+
+    if (lastMsgId !== msgIdEl.textContent) {
+      lastMsgId = msgIdEl.textContent;
+      const contentEl = messageEl.querySelector('.message-content');
+      modal.contentEl.value = contentEl.textContent;
+      modal.headerEl.textContent = msgIdEl.textContent;
+      modal.formEl.action = `/user/${userId}/conversations/${targetConvId}/${msgIdEl.textContent}?_method=PUT`;
+    };
+
+    modal.el.show();
+    return;
+  };
+
+  if (previousEl !== messageEl) { // show/hide edit and delete buttons
     const delBtn = getDeleteButton();
     if (delBtn) delBtn.remove();
     const editBtn = getEditButton();
@@ -47,6 +80,12 @@ messageAreaEl.addEventListener("click", (evt) => {
       const newEditBtn = newButton('button', 'edit-btn', 'Edit');
       messageEl.appendChild(newEditBtn);
       messageEl.appendChild(newDelBtn);
-    }
+    };
   };
 });
+
+// function evtEditBtnClick(evt){
+//   const target = evt.target;
+//   if(target.id !== 'edit-btn') return;
+//   editModal.showModal();
+// };
