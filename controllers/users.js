76d8bce211e6d54ac9d4654705f2ecb5c7c70140
dmacedo <devinm3333@outlook.com>
2025-08-05 12:26:21 -0700

    allow showing user page without pre-existing conversations

--- controllers/users.js
+++ controllers/users.js
@@ -7,10 +7,15 @@ const router = express.Router();
 // extract helper functions
 // const getConversationName = helpers.getConversationName;
 const createSendConvObj = helpers.createSendConvObj;
-
+let sendObj;
 router.get('/:id', async (req, res) => { // load user page for the first time
-  const sendObj = await createSendConvObj(req);
-  res.render('user/index.ejs', sendObj);
+  const user = await User.findById(req.session.user._id);
+  if (user.conversations.length) {
+    sendObj = await createSendConvObj(req);
+  } else {
+    sendObj = {targetConversation: null, conversations: null};
+  };
+    res.render('user/index.ejs', sendObj);
 });
 
 module.exports = router;
